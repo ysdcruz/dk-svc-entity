97660078b66fbf45c434f9f886eae7c4a3e7a4f3
Ysabel Dela Cruz <delacruz.ysa@gmail.com>
2022-05-04 15:43:45 +0800

    Migrated to H2

--- src/main/java/com/devkinetics/svc/entity/Application.java
+++ src/main/java/com/devkinetics/svc/entity/Application.java
@@ -2,7 +2,6 @@ package com.devkinetics.svc.entity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 // Config from YML is not disabling the security during test run but can be disabled if via mvn spring-boot run.
 @SpringBootApplication
